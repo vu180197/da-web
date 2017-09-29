6f3c4a221b7740b648e9ca568acdb8952e66ba14
nhAnhVu <32349079+nhAnhVu@users.noreply.github.com>
2017-09-29 10:07:13 +0700

    database
    data

---
+++ database.php
@@ -0,0 +1,40 @@
+<?php
+
+    private $host       ="";
+    private $username   ="";
+    private $password   ="";
+    private $dbname     ="";
+
+    private $conn   ="";
+    private $result ="";
+    
+    //Hàm kết nối csdl
+    function Connect(){
+
+    }
+
+    // Hàm ngắt kết nối csdl
+    function Disconnect(){
+        
+    }
+
+     // Hàm thực thi câu truy vấn
+     function Query(){
+        
+    }
+
+     // Hàm đếm số dòng trả về khi thực hiện truy vấn
+     function numRows(){
+        
+    }
+
+     // Hàm lấy 1 dòng dữ liệu từ câu SELECT
+     function fetch(){
+        
+    }
+
+     // Hàm lấy tất cả dòng dữ liệu từ câu SELECT
+     function fetchAll(){
+        
+    }
+?>
